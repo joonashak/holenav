3e0d9704a2561f455c8c720644191d89de6c8adb
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-16 15:11:48 +0300

    Huoh I don't even env var

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default () => {
   const onClick = async () =>
-    axios.get("http://localhost:3001/auth/cookie-test", {
+    axios.get("http://65.21.180.194/auth/cookie-test", {
       withCredentials: true,
     });
 
