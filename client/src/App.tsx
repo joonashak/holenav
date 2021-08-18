f56d0c7afb4f493daa97b6d90033b6ac7a8027f6
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-18 23:18:17 +0300

    Refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,23 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { Container, makeStyles, Theme, ThemeProvider } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core";
 import { AuthProvider } from "./auth/useAuth";
 import Router from "./components/Router";
+import ViewportContainer from "./components/ViewportContainer";
 import appTheme from "./theme";
-
-const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    backgroundColor: theme.palette.primary.main,
-    minHeight: "100vh",
-  },
-}));
 
 const apolloClient = new ApolloClient({
   uri: `${process.env.REACT_APP_CMS_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
-export default () => {
-  const classes = useStyles();
-
-  return (
-    <ThemeProvider theme={appTheme}>
-      <AuthProvider>
-        <ApolloProvider client={apolloClient}>
-          <Container
-            maxWidth={false}
-            disableGutters
-            className={classes.container}
-          >
-            <Router />
-          </Container>
-        </ApolloProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  );
-};
+export default () => (
+  <ThemeProvider theme={appTheme}>
+    <AuthProvider>
+      <ApolloProvider client={apolloClient}>
+        <ViewportContainer>
+          <Router />
+        </ViewportContainer>
+      </ApolloProvider>
+    </AuthProvider>
+  </ThemeProvider>
+);
---
+++ client/src/components/ViewportContainer.tsx
@@ -0,0 +1,23 @@
+import { Container, makeStyles, Theme } from "@material-ui/core";
+import { ReactChild } from "react";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    backgroundColor: theme.palette.primary.main,
+    minHeight: "100vh",
+  },
+}));
+
+interface ViewportContainerProps {
+  children: ReactChild;
+}
+
+export default ({ children }: ViewportContainerProps) => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth={false} disableGutters className={classes.container}>
+      {children}
+    </Container>
+  );
+};
