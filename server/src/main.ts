9f28f0b673adabe08555bac45f29b5741bebfc44
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-18 18:24:10 +0300

    Remove data test

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import eff from "../assets/staticData/wormholeEffects.json";
 
-console.log("data test:", eff);
 const port = process.env.PORT || 3001;
 
 async function bootstrap() {
