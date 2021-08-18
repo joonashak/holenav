e54201c584a93aa0407fcd6dbb28475903c24bff
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-18 19:21:35 +0300

    Super (too) simple data migration

--- server/src/dataMigration/dataMigration.module.ts
+++ server/src/dataMigration/dataMigration.module.ts
@@ -1,5 +1,10 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { System, SystemSchema } from "../entities/system/system.model";
 import { DataMigrationService } from "./dataMigration.service";
 
-@Module({ providers: [DataMigrationService] })
+@Module({
+  imports: [MongooseModule.forFeature([{ name: System.name, schema: SystemSchema }])],
+  providers: [DataMigrationService],
+})
 export class DataMigrationModule {}
--- server/src/dataMigration/dataMigration.service.ts
+++ server/src/dataMigration/dataMigration.service.ts
@@ -1,8 +1,27 @@
-import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
+import { Injectable, Logger, OnApplicationBootstrap } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { System, SystemDocument } from "../entities/system/system.model";
+import systems from "../../assets/staticData/systems.json";
 
 @Injectable()
 export class DataMigrationService implements OnApplicationBootstrap {
-  onApplicationBootstrap() {
-    console.log("bootstrappinggg");
+  private readonly logger = new Logger(DataMigrationService.name);
+
+  constructor(@InjectModel(System.name) private systemModel: Model<SystemDocument>) {}
+
+  async onApplicationBootstrap() {
+    this.logger.log("Starting data migration...");
+
+    const ops = systems.map(({ name, ...rest }) => ({
+      updateOne: {
+        filter: { name },
+        update: { ...rest },
+        upsert: true,
+      },
+    }));
+    await this.systemModel.bulkWrite(ops);
+
+    this.logger.log("Data migration finished.");
   }
 }
