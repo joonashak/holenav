d06def718468d533b0912366db11dd82228859f7
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-25 22:54:56 +0300

    Fix mui theme error

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@material-ui/core";
+import { unstable_createMuiStrictModeTheme as createTheme } from "@material-ui/core";
 import { blueGrey, grey } from "@material-ui/core/colors";
 
 export default createTheme({
