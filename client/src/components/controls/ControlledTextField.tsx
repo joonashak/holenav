374873402544a41832604f90075784662a01c18f
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-29 20:18:12 +0300

    Create controlled text input

---
+++ client/src/components/controls/ControlledTextField.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { TextField } from "@material-ui/core";
+import { Controller } from "react-hook-form";
+
+type ControlledTextFieldProps = {
+  name: string;
+  control: any;
+  label: string;
+};
+
+export default ({ name, control, label }: ControlledTextFieldProps) => {
+  const Render = ({ field }: any) => (
+    <TextField {...field} label={label} variant="outlined" />
+  );
+
+  return (
+    <Controller name={name} control={control} render={Render} defaultValue="" />
+  );
+};
