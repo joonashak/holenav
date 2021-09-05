bd92a954d94b055b6965492c0522a2ef0fa1a703
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-09-05 12:22:04 +0300

    More CI wrestling

--- scripts/run-tests-ci.sh
+++ scripts/run-tests-ci.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-ci.yml build server-ci client-ci e2e-ci
-docker-compose -f docker-compose-ci.yml up -d db-ci server-ci client-ci
+# Run server linter in dev container, because dev deps are pruned from production container.
+docker-compose build server || exit 1
+docker-compose run --no-deps server npm run lint || exit 1
 
-# Linting
-docker-compose -f docker-compose-ci.yml exec -T server-ci npm run lint || exit 1
-docker-compose -f docker-compose-ci.yml exec -T client-ci npm run lint || exit 1
+# Build and lint client.
+docker-compose -f docker-compose-ci.yml build client-ci || exit 1
+docker-compose -f docker-compose-ci.yml run client-ci npm run lint || exit 1
+
+# Start services.
+docker-compose -f docker-compose-ci.yml build server-ci || exit 1
+docker-compose -f docker-compose-ci.yml up -d db-ci server-ci client-ci || exit 1
+
+# Run linter for test code and E2E tests.
+docker-compose -f docker-compose-ci.yml build e2e-ci || exit 1
 docker-compose -f docker-compose-ci.yml run e2e-ci npm run lint || exit 1
-
-# E2E
 docker-compose -f docker-compose-ci.yml run e2e-ci || exit 1
 
 # This is not strictly necessary in CI, but helps when debugging things locally, etc.
