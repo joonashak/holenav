e5801df39ef515c41153b2826e405b433e4305e3
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-10-17 11:51:45 +0300

    Select styles

--- client/src/components/controls/Select/index.tsx
+++ client/src/components/controls/Select/index.tsx
@@ -15,35 +15,47 @@ type SelectProps = {
   title: string;
 };
 
-const useLabelStyles = makeStyles((theme: Theme) => ({
-  root: {
+const useStyles = makeStyles((theme: Theme) => ({
+  label: {
     color: theme.palette.secondary.light,
     "&.Mui-focused": {
       color: theme.palette.secondary.main,
     },
   },
-}));
-
-const useSelectStyles = makeStyles((theme: Theme) => ({
-  root: {
-    borderBottom: `2px solid ${theme.palette.secondary.light}`,
+  select: {
+    "&:before": {
+      borderBottom: `1px solid ${theme.palette.secondary.main}`,
+    },
+    "&:not(.Mui-disabled):hover::before": {
+      borderBottom: `1px solid ${theme.palette.secondary.light}`,
+    },
+    "&:after": {
+      borderBottom: `1px solid ${theme.palette.secondary.light}`,
+    },
+  },
+  outlined: {
+    border: `1px solid ${theme.palette.secondary.dark}`,
+  },
+  arrowIcon: {
+    color: theme.palette.secondary.main,
   },
 }));
 
 const Select = ({ options, onChange, value, title }: SelectProps) => {
-  const labelClasses = useLabelStyles();
-  const selectClasses = useSelectStyles();
+  const classes = useStyles();
 
   return (
     <FormControl fullWidth>
-      <InputLabel id={`select-${title}-label`} classes={labelClasses}>
+      <InputLabel id={`select-${title}-label`} className={classes.label}>
         {title}
       </InputLabel>
       <MuiSelect
         value={value}
         labelId={`select-${title}-label`}
         onChange={onChange}
-        classes={selectClasses}
+        className={classes.select}
+        inputProps={{ classes: { icon: classes.arrowIcon, outlined: classes.outlined } }}
+        variant="outlined"
       >
         {options.map((opt) => (
           <MenuItem key={`select-${title}-option-${opt.id}`} value={opt.value}>
