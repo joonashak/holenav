a9ce75d60a349cde6fb8b0af8a8070eca4662a91
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-10-23 21:06:34 +0300

    Refactor tests

--- e2e/cypress/support/helpers/wormholeForm.utils.ts
+++ e2e/cypress/support/helpers/wormholeForm.utils.ts
@@ -11,31 +11,22 @@ type TestableWormholeProps = {
   eveId?: string;
 };
 
+const wormholePropertyTests = {
+  name: (name: string) => cy.cs("textfield-name").should("have.value", name),
+  destinationName: (name: string) =>
+    cy.get("[data-cy=autocomplete-destinationName-input] > div > input").should("have.value", name),
+  type: (type: string) =>
+    cy.get("[data-cy=select-whType] > div > input").should("have.value", type),
+  eveId: (eveId: string) =>
+    cy.get("[data-cy=textfield-eveId] > div > input").should("have.value", eveId),
+};
+
 export const testWormholeProperties = (props: TestableWormholeProps, url: string): void => {
   const { name } = props;
   cy.visit(url);
   cy.get("#scanning-content").contains(name);
   cy.cs(`edit-sig-${name}`).click();
 
-  cy.cs("textfield-name").should("have.value", name);
-
-  // Test optional properties.
-  Object.keys(props).forEach((key) => {
-    if (key === "destinationName") {
-      cy.get("[data-cy=autocomplete-destinationName-input] > div > input").should(
-        "have.value",
-        props[key]
-      );
-    }
-
-    if (key === "type") {
-      cy.get("[data-cy=select-whType] > div > input").should("have.value", props[key]);
-    }
-
-    if (key === "eveId") {
-      cy.get("[data-cy=textfield-eveId] > div > input").should("have.value", props[key]);
-    }
-
-    // TODO: Add farside wh type check.
-  });
+  Object.keys(props).forEach((key) => wormholePropertyTests[key](props[key]));
+  // TODO: Add farside wh type check.
 };
