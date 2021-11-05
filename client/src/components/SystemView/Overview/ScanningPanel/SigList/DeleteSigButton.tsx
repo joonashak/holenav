a1bb239c3b429b676f6bddc46e34837739f54ddf
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-11-05 13:40:40 +0200

    Add confirmation to sig delete

--- client/src/components/SystemView/Overview/ScanningPanel/SigList/DeleteSigButton.tsx
+++ client/src/components/SystemView/Overview/ScanningPanel/SigList/DeleteSigButton.tsx
@@ -1,4 +1,5 @@
-import { IconButton } from "@mui/material";
+import { MouseEvent, useState } from "react";
+import { IconButton, Menu, MenuItem } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import useSystemData, { Signature, Wormhole } from "../../../SystemData/useSystemData";
 import useNotification from "../../../../GlobalNotification/useNotification";
@@ -7,13 +8,21 @@ type DeleteSigButtonProps = {
   sig: Signature | Wormhole;
 };
 
-// TODO: Add confirmation before acual delete.
 const DeleteSigButton = ({ sig }: DeleteSigButtonProps) => {
+  const [anchor, setAnchor] = useState<Element | null>(null);
   const { deleteSignature, deleteWormhole } = useSystemData();
   const { setNotification } = useNotification();
   const isWormhole = Object.keys(sig).includes("eol");
 
-  const onClick = async () => {
+  const onOpen = (event: MouseEvent<HTMLElement>) => {
+    setAnchor(event.target as Element);
+  };
+
+  const onClose = () => {
+    setAnchor(null);
+  };
+
+  const onConfirm = async () => {
     if (isWormhole) {
       await deleteWormhole(sig.id);
       setNotification("Wormhole deleted.", "success");
@@ -21,17 +30,24 @@ const DeleteSigButton = ({ sig }: DeleteSigButtonProps) => {
       await deleteSignature(sig.id);
       setNotification("Signature deleted.", "success");
     }
+
+    onClose();
   };
 
   return (
-    <IconButton
-      onClick={onClick}
-      size="small"
-      aria-label="Delete Signature"
-      data-cy={`delete-sig-${sig.name}`}
-    >
-      <DeleteIcon fontSize="inherit" />
-    </IconButton>
+    <>
+      <IconButton
+        onClick={onOpen}
+        size="small"
+        aria-label="Delete Signature"
+        data-cy={`delete-sig-${sig.name}`}
+      >
+        <DeleteIcon fontSize="inherit" />
+      </IconButton>
+      <Menu open={!!anchor} anchorEl={anchor} onClose={onClose}>
+        <MenuItem onClick={onConfirm}>Confirm</MenuItem>
+      </Menu>
+    </>
   );
 };
 
