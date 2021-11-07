6a8f5a1d91d6028833dd0730e0084411834f9e06
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-11-07 23:32:11 +0200

    Style quick control widget

--- client/src/components/SystemView/QuickControl/index.tsx
+++ client/src/components/SystemView/QuickControl/index.tsx
@@ -1,8 +1,23 @@
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
+import {
+  SpeedDial,
+  SpeedDialAction as MuiSpeedDialAction,
+  SpeedDialActionProps,
+} from "@mui/material";
 import GroupIcon from "@mui/icons-material/Group";
 import MapIcon from "@mui/icons-material/Map";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+const SpeedDialAction = (props: SpeedDialActionProps) => (
+  <MuiSpeedDialAction
+    {...props}
+    FabProps={{
+      sx: {
+        bgcolor: "secondary.dark",
+        "&:hover": { bgcolor: "secondary.main" },
+      },
+    }}
+  />
+);
 
 const QuickControl = () => {
   console.log("asd");
@@ -10,9 +25,16 @@ const QuickControl = () => {
   return (
     <SpeedDial
       ariaLabel="Quick Control Panel"
-      sx={{ position: "absolute", top: 16, right: 16 }}
-      icon={<SpeedDialIcon />}
       direction="left"
+      sx={{ position: "absolute", top: 16, right: 16 }}
+      icon={<SettingsIcon sx={{ fontSize: 32 }} />}
+      FabProps={{
+        sx: {
+          bgcolor: "primary.main",
+          color: "primary.dark",
+          "&:hover": { bgcolor: "primary.main" },
+        },
+      }}
     >
       <SpeedDialAction icon={<GroupIcon />} tooltipTitle="Select Character" />
       <SpeedDialAction icon={<MapIcon />} tooltipTitle="Select map" />
