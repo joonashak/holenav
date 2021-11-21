54414e155ece8bfd98db4adc5198dd77e042b610
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-11-21 15:14:34 +0200

    Parse paste data into something useful

--- client/src/components/SystemView/Overview/ScanningPanel/useSigPasteListener.tsx
+++ client/src/components/SystemView/Overview/ScanningPanel/useSigPasteListener.tsx
@@ -1,6 +1,56 @@
+import SigTypes from "../../../../enum/SigTypes";
+import useNotification from "../../../GlobalNotification/useNotification";
+
+type PasteData = {
+  eveId: string;
+  type: SigTypes | null;
+};
+
 const useSigPasteListener = () => {
+  const { showWarningNotification } = useNotification();
+
+  const findSigType = (typeString: string) => {
+    if (typeString === "Data Site") {
+      return SigTypes.DATA;
+    }
+    if (typeString === "Relic Site") {
+      return SigTypes.RELIC;
+    }
+    if (typeString.match(/Wormhole/)) {
+      return SigTypes.WORMHOLE;
+    }
+    return null;
+  };
+
+  const getFormattedPasteData = (pasteEvent: ClipboardEvent): Array<PasteData> => {
+    const input = pasteEvent.clipboardData?.getData("text") || "";
+    const rows = input.split(/\r?\n/);
+    const splitRows = rows.map((row) => row.split(/\t+/));
+
+    const formattedData = splitRows.map((row) => {
+      const eveId = row[0];
+      if (!eveId.match(/^[A-Z]{3}-\d{3}$/)) {
+        throw new Error("Bad paste data detected. No changes were made.");
+      }
+
+      const type = findSigType(row[1]);
+      return { eveId, type };
+    });
+
+    return formattedData;
+  };
+
   const sigPasteListener = (event: Event) => {
-    console.log((event as ClipboardEvent).clipboardData?.getData("text"));
+    let data: Array<PasteData> = [];
+
+    try {
+      data = getFormattedPasteData(event as ClipboardEvent);
+    } catch (error: any) {
+      showWarningNotification(error.message, { autoHide: true });
+      return;
+    }
+
+    console.log("data", data);
   };
 
   return {
