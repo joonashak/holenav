a4c1cdf7e8805716c443d2ca1bab2841cf1db822
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-01-08 00:43:55 +0200

    Add expand icon and disabled state to effect panel

--- client/src/components/SystemView/Overview/SystemInfo/EffectInfoAccordion.tsx
+++ client/src/components/SystemView/Overview/SystemInfo/EffectInfoAccordion.tsx
@@ -1,15 +1,28 @@
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import useSystemData from "../../SystemData/useSystemData";
 import EffectInfoTable from "./EffectInfoTable";
 
 const EffectInfoAccordion = () => {
   const { effect } = useSystemData();
 
+  const ExpandIcon = () =>
+    effect ? <ExpandMoreIcon sx={{ color: "primary.light", m: 0 }} /> : null;
+
   return (
-    <Accordion disableGutters defaultExpanded>
-      <AccordionSummary sx={{ minHeight: 0, "&>*": { m: 1, justifyContent: "center" } }}>
+    <Accordion sx={{ width: 0.8, mx: "auto" }} disableGutters>
+      <AccordionSummary
+        expandIcon={<ExpandIcon />}
+        disabled={!effect}
+        aria-controls="system-effects-accordion"
+        id="system-effects-header"
+        sx={{
+          minHeight: 0,
+          "&>div:first-of-type": { m: 1, justifyContent: "center", flexGrow: 0 },
+        }}
+      >
         <Typography sx={{ textTransform: "uppercase" }} variant="button">
-          System Effect
+          {effect ? "Effect Details" : "No System Effect"}
         </Typography>
       </AccordionSummary>
       <AccordionDetails sx={{ p: 0 }}>
