ac4ec9d454eeaa384b0cb3d6408b4b9bfcf22944
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-02-11 19:36:41 +0200

    Outline settings menu layout

--- client/src/components/SettingsView/SettingsMenu.tsx
+++ client/src/components/SettingsView/SettingsMenu.tsx
@@ -1,9 +1,57 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import GroupIcon from "@mui/icons-material/Group";
+import { ReactNode } from "react";
+
+type MenuDividerProps = {
+  children: ReactNode;
+};
+
+const MenuDivider = ({ children }: MenuDividerProps) => (
+  <Divider
+    sx={{
+      color: "secondary.light",
+      "&::before, &::after": {
+        borderColor: "secondary.dark",
+        zIndex: 0,
+      },
+    }}
+  >
+    <Typography variant="h3">{children}</Typography>
+  </Divider>
+);
 
 const SettingsMenu = () => (
-  <Box sx={{ bgcolor: "primary.main", maxWidth: "20rem", height: "100vh" }}>
-    <nav aria-label="Settings Navigation Menu">
+  <nav aria-label="Personal Settings Navigation Menu">
+    <Box
+      sx={{
+        bgcolor: "primary.main",
+        color: "primary.contrastText",
+        maxWidth: "20rem",
+        height: "100vh",
+        pt: 5,
+      }}
+    >
+      <MenuDivider>Personal Settings</MenuDivider>
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <GroupIcon />
+            </ListItemIcon>
+            <ListItemText primary="Folders" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <MenuDivider>Admin Settings</MenuDivider>
       <List>
         <ListItem>
           <ListItemButton>
@@ -14,8 +62,18 @@ const SettingsMenu = () => (
           </ListItemButton>
         </ListItem>
       </List>
-    </nav>
-  </Box>
+      <List sx={{ position: "absolute", bottom: 0 }}>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <GroupIcon />
+            </ListItemIcon>
+            <ListItemText primary="Back To App" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  </nav>
 );
 
 export default SettingsMenu;
