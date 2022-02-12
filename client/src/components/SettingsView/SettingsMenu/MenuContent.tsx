d56eb8e2cff2e5d1ce5e7f6d687c77d3d51bfbfc
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-02-12 15:42:12 +0200

    Hide settings menu behind drawer in mobile

---
+++ client/src/components/SettingsView/SettingsMenu/MenuContent.tsx
@@ -0,0 +1,54 @@
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import GroupIcon from "@mui/icons-material/Group";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MenuDivider from "./MenuDivider";
+
+const MenuContent = () => (
+  <nav aria-label="Personal Settings Navigation Menu">
+    <Box
+      sx={{
+        bgcolor: "primary.main",
+        color: "primary.contrastText",
+        maxWidth: "20rem",
+        //height: "100vh",
+        pt: 5,
+      }}
+    >
+      <MenuDivider>Personal Settings</MenuDivider>
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsIcon color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary="General" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <MenuDivider>Admin Settings</MenuDivider>
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <GroupIcon color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <List sx={{ position: "absolute", bottom: 0, bgcolor: "primary.light" }}>
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <ArrowBackIcon color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary="Back To App" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  </nav>
+);
+
+export default MenuContent;
--- client/src/components/SettingsView/SettingsMenu/index.tsx
+++ client/src/components/SettingsView/SettingsMenu/index.tsx
@@ -1,54 +1,33 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import GroupIcon from "@mui/icons-material/Group";
-import SettingsIcon from "@mui/icons-material/Settings";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import MenuDivider from "./MenuDivider";
+import { useState } from "react";
+import { AppBar, Drawer, IconButton, Toolbar, useMediaQuery, useTheme } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuContent from "./MenuContent";
 
-const SettingsMenu = () => (
-  <nav aria-label="Personal Settings Navigation Menu">
-    <Box
-      sx={{
-        bgcolor: "primary.main",
-        color: "primary.contrastText",
-        maxWidth: "20rem",
-        height: "100vh",
-        pt: 5,
-      }}
-    >
-      <MenuDivider>Personal Settings</MenuDivider>
-      <List>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <SettingsIcon color="secondary" />
-            </ListItemIcon>
-            <ListItemText primary="General" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-      <MenuDivider>Admin Settings</MenuDivider>
-      <List>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <GroupIcon color="secondary" />
-            </ListItemIcon>
-            <ListItemText primary="Users" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-      <List sx={{ position: "absolute", bottom: 0, bgcolor: "primary.light" }}>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <ArrowBackIcon color="secondary" />
-            </ListItemIcon>
-            <ListItemText primary="Back To App" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Box>
-  </nav>
-);
+const SidebarMenu = () => <MenuContent />;
+
+const DrawerMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton aria-label="Open Settings Menu" onClick={toggleMenu} size="large">
+          <MenuIcon />
+        </IconButton>
+        <Drawer anchor="top" open={menuOpen} onClose={toggleMenu}>
+          <MenuContent />
+        </Drawer>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+const SettingsMenu = () => {
+  const theme = useTheme();
+  const wideViewport = useMediaQuery(theme.breakpoints.up("sm"), { noSsr: true });
+
+  return wideViewport ? <SidebarMenu /> : <DrawerMenu />;
+};
 
 export default SettingsMenu;
