93a97a632ff5b121438d1250fb8ef24ecca6af05
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-02-17 16:10:57 +0200

    Refactor

--- client/src/components/SettingsView/SettingsMenu/MenuContent.tsx
+++ client/src/components/SettingsView/SettingsMenu/MenuContent.tsx
@@ -7,6 +7,7 @@ import {
   ListItemIcon,
   ListItemText,
   ListProps,
+  SvgIcon,
 } from "@mui/material";
 import GroupIcon from "@mui/icons-material/Group";
 import SettingsIcon from "@mui/icons-material/Settings";
@@ -14,6 +15,22 @@ import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 import FolderSharedIcon from "@mui/icons-material/FolderShared";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import MenuDivider from "./MenuDivider";
+
+type MenuItemProps = {
+  text: string;
+  Icon: typeof SvgIcon;
+};
+
+const MenuItem = ({ text, Icon }: MenuItemProps) => (
+  <ListItem>
+    <ListItemButton>
+      <ListItemIcon>
+        <Icon color="secondary" />
+      </ListItemIcon>
+      <ListItemText primary={text} />
+    </ListItemButton>
+  </ListItem>
+);
 
 type MenuContentProps = {
   sx?: BoxProps["sx"];
@@ -32,44 +49,16 @@ const MenuContent = ({ sx, bottomListSx }: MenuContentProps) => (
     >
       <MenuDivider>Personal Settings</MenuDivider>
       <List>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <SettingsIcon color="secondary" />
-            </ListItemIcon>
-            <ListItemText primary="General" />
-          </ListItemButton>
-        </ListItem>
+        <MenuItem text="General" Icon={SettingsIcon} />
       </List>
       <MenuDivider>Folder Settings</MenuDivider>
       <List>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <FolderOpenIcon color="secondary" />
-            </ListItemIcon>
-            <ListItemText primary="Active Folder" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <FolderSharedIcon color="secondary" />
-            </ListItemIcon>
-            <ListItemText primary="Folder Management" />
-          </ListItemButton>
-        </ListItem>
+        <MenuItem text="Active Folder" Icon={FolderOpenIcon} />
+        <MenuItem text="Folder Management" Icon={FolderSharedIcon} />
       </List>
       <MenuDivider>Admin Settings</MenuDivider>
       <List>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <GroupIcon color="secondary" />
-            </ListItemIcon>
-            <ListItemText primary="Users" />
-          </ListItemButton>
-        </ListItem>
+        <MenuItem text="Users" Icon={GroupIcon} />
       </List>
       <List sx={{ bgcolor: "primary.light", ...bottomListSx }}>
         <ListItem>
