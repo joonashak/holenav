8edc61ab7498028450a1639b89d2950bb2ba908d
joonashak <hakkijoo@gmail.com>
2022-04-06 23:42:57 +0300

    Add notifications to createFolder

--- client/src/components/SettingsView/SettingsData/useSettingsData.ts
+++ client/src/components/SettingsView/SettingsData/useSettingsData.ts
@@ -1,15 +1,19 @@
 import { useMutation } from "@apollo/client";
 import { Downgraded, useState } from "@hookstate/core";
 import { settingsState } from ".";
+import useNotification from "../../GlobalNotification/useNotification";
 import { CREATE_FOLDER } from "./graphql";
 
 const useSettingsData = () => {
   const state = useState(settingsState);
+  const { showInfoNotification, showErrorNotification } = useNotification();
 
   const [createFolderMutation] = useMutation(CREATE_FOLDER, {
     onCompleted: ({ createFolder }) => {
       state.accessibleFolders.set((folders) => folders.concat([createFolder]));
+      showInfoNotification("Folder created.");
     },
+    onError: () => showErrorNotification("Could not create new folder."),
   });
 
   const createFolder = async (name: string) => createFolderMutation({ variables: { name } });
