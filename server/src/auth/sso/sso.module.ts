f883e4563d64f615211118ed74b06900c9eb20cb
joonashak <hakkijoo@gmail.com>
2022-04-17 19:41:24 +0300

    Refactor SSO API calls into a new service

--- server/src/auth/sso/sso.module.ts
+++ server/src/auth/sso/sso.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { CharacterModule } from "../../entities/character/character.module";
 import SsoResolver from "./sso.resolver";
 import { SsoService } from "./sso.service";
+import { SsoApiService } from "./ssoApi.service";
 import { SsoSessionModule } from "./ssoSession/ssoSession.module";
 
 @Module({
   imports: [SsoSessionModule, CharacterModule],
-  providers: [SsoService, SsoResolver],
-  exports: [SsoService],
+  providers: [SsoService, SsoResolver, SsoApiService],
+  exports: [SsoService, SsoApiService],
 })
 export class SsoModule {}
--- server/src/auth/sso/sso.service.ts
+++ server/src/auth/sso/sso.service.ts
@@ -1,16 +1,9 @@
 import { Injectable } from "@nestjs/common";
-import axios from "axios";
-import FormData from "form-data";
-import {
-  clientUrl,
-  getClientLoginCallbackUrl,
-  ssoCallbackUrl,
-  ssoClientId,
-  ssoSecretKey,
-} from "../../config";
+import { clientUrl, getClientLoginCallbackUrl, ssoCallbackUrl, ssoClientId } from "../../config";
 import { CharacterService } from "../../entities/character/character.service";
 import { User } from "../../user/user.model";
 import { UserService } from "../../user/user.service";
+import { SsoApiService } from "./ssoApi.service";
 import { SsoSessionService } from "./ssoSession/ssoSession.service";
 import SsoSessionTypes from "./ssoSession/ssoSessionTypes.enum";
 import { SsoUrl } from "./ssoUrl.enum";
@@ -21,6 +14,7 @@ export class SsoService {
     private ssoSessionService: SsoSessionService,
     private characterService: CharacterService,
     private userService: UserService,
+    private ssoApiService: SsoApiService,
   ) {}
 
   /**
@@ -33,41 +27,6 @@ export class SsoService {
   }
 
   /**
-   * Get access and refresh tokens from EVE SSO server.
-   */
-  async getSsoTokens(authorizationCode: string) {
-    const formData = new FormData();
-    formData.append("grant_type", "authorization_code");
-    formData.append("code", authorizationCode);
-
-    const res = await axios.post(SsoUrl.Token, formData, {
-      auth: { username: ssoClientId, password: ssoSecretKey },
-      headers: { "Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}` },
-    });
-
-    const { data } = res;
-
-    return {
-      accessToken: data.access_token,
-      refreshToken: data.refresh_token,
-    };
-  }
-
-  /**
-   * Verify JWT token through EVE SSO.
-   *
-   * Returns character data if the token is valid.
-   */
-  async verifyAndDecodeToken(token: string) {
-    // Implicitly fails on non-200 status code.
-    const { data } = await axios.get(SsoUrl.Verify, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-
-    return data;
-  }
-
-  /**
    * Get the SSO tokens using the authorization code supplied by the user, save
    * them and update the SSO state with the character's ID.
    *
@@ -76,8 +35,8 @@ export class SsoService {
   async handleCallback(authorizationCode: string, state: string): Promise<string> {
     const ssoSession = await this.ssoSessionService.verifySsoSession(state);
 
-    const { accessToken, refreshToken } = await this.getSsoTokens(authorizationCode);
-    const jwtData = await this.verifyAndDecodeToken(accessToken);
+    const { accessToken, refreshToken } = await this.ssoApiService.getSsoTokens(authorizationCode);
+    const jwtData = await this.ssoApiService.verifyAndDecodeSsoAccessToken(accessToken);
 
     const character = await this.characterService.upsert({
       name: jwtData.CharacterName,
