d528eb3f899b63c54bc0aa1882f8014a2fbc0d75
joonashak <hakkijoo@gmail.com>
2022-05-08 18:53:28 +0300

    Add role select to folder management

--- client/src/components/SettingsView/folder/FolderManagement/ManageFolder/index.tsx
+++ client/src/components/SettingsView/folder/FolderManagement/ManageFolder/index.tsx
@@ -1,5 +1,6 @@
 import { createState, useState } from "@hookstate/core";
-import { Character } from "../../../../../generated/graphqlOperations";
+import { Stack } from "@mui/material";
+import { Character, FolderRoles } from "../../../../../generated/graphqlOperations";
 import Select from "../../../../controls/Select";
 import useSettingsData from "../../../SettingsData/useSettingsData";
 import CharacterSearch from "./CharacterSearch";
@@ -7,20 +8,27 @@ import CharacterSearch from "./CharacterSearch";
 type ManageFolderState = {
   selectedFolder: string;
   selectedCharacter: Character | null;
+  selectedRole: string;
 };
 
 export const manageFolderState = createState<ManageFolderState>({
   selectedFolder: "",
   selectedCharacter: null,
+  selectedRole: "",
 });
 
 const ManageFolder = () => {
-  const { selectedFolder } = useState(manageFolderState);
+  const { selectedFolder, selectedRole } = useState(manageFolderState);
   const { manageableFolders } = useSettingsData();
   const folderOptions = manageableFolders.map(({ id, name }) => ({ id, value: id, label: name }));
+  const roleOptions = Object.keys(FolderRoles).map((role) => ({
+    id: `role-${role}`,
+    value: role,
+    label: role,
+  }));
 
   return (
-    <>
+    <Stack spacing={2}>
       <Select
         options={folderOptions}
         onChange={(event) => selectedFolder.set(event.target.value)}
@@ -28,7 +36,13 @@ const ManageFolder = () => {
         value={selectedFolder.value}
       />
       <CharacterSearch />
-    </>
+      <Select
+        options={roleOptions}
+        onChange={(event) => selectedRole.set(event.target.value)}
+        title="Select Role"
+        value={selectedRole.value}
+      />
+    </Stack>
   );
 };
 
