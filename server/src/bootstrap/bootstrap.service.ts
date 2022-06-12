033eff641542732320208b059b9e1b439d78f8de
joonashak <hakkijoo@gmail.com>
2022-06-12 18:50:48 +0300

    Call CD webhook to deploy client when app is updated

--- server/src/bootstrap/bootstrap.service.ts
+++ server/src/bootstrap/bootstrap.service.ts
@@ -1,6 +1,13 @@
 import { Injectable, Logger, OnApplicationBootstrap } from "@nestjs/common";
+import { request } from "@octokit/request";
 import { compare } from "compare-versions";
-import { APP_VERSION } from "../config";
+import {
+  APP_VERSION,
+  CLIENT_CD_OWNER,
+  CLIENT_CD_REPO,
+  CLIENT_CD_TOKEN,
+  CLIENT_CD_WORKFLOW_ID,
+} from "../config";
 import { AppDataService } from "../entities/app-data/app-data.service";
 
 @Injectable()
@@ -13,6 +20,7 @@ export class BootstrapService implements OnApplicationBootstrap {
     if (await this.appUpdated()) {
       this.logger.log("Application update detected.");
       await this.updateAppDataVersion();
+      await this.triggerClientDeployment();
     }
   }
 
@@ -28,5 +36,35 @@ export class BootstrapService implements OnApplicationBootstrap {
 
   private async updateAppDataVersion(): Promise<void> {
     await this.appDataService.updateAppData({ appVersion: APP_VERSION });
+  }
+
+  /**
+   * Trigger client deployment using a GitHub Actions webhook.
+   */
+  private async triggerClientDeployment(): Promise<void> {
+    if (!CLIENT_CD_TOKEN) {
+      this.logger.log("Skipping client deployment (CLIENT_CD_TOKEN not set).");
+      return;
+    }
+
+    const url = "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches";
+    const options = {
+      headers: {
+        authorization: `token ${CLIENT_CD_TOKEN}`,
+      },
+      owner: CLIENT_CD_OWNER,
+      repo: CLIENT_CD_REPO,
+      workflow_id: CLIENT_CD_WORKFLOW_ID,
+      ref: `v${APP_VERSION}`,
+    };
+
+    try {
+      await request(url, options);
+    } catch (error) {
+      this.logger.warn("Request to client deployment webhook failed with the following error:");
+      throw error;
+    }
+
+    this.logger.log("Client deployment triggered.");
   }
 }
--- server/src/config.ts
+++ server/src/config.ts
@@ -11,5 +11,9 @@ export const jwtSecret = process.env.JWT_SECRET;
 export const jwtLifetime = process.env.JWT_LIFETIME || "30d";
 export const devToolsEnabled = process.env.ENABLE_DEVTOOLS === "true";
 export const APP_VERSION = process.env.npm_package_version;
+export const CLIENT_CD_TOKEN = process.env.CLIENT_CD_TOKEN;
+export const CLIENT_CD_OWNER = process.env.CLIENT_CD_OWNER;
+export const CLIENT_CD_REPO = process.env.CLIENT_CD_REPO;
+export const CLIENT_CD_WORKFLOW_ID = process.env.CLIENT_CD_WORKFLOW_ID;
 
 export const getClientLoginCallbackUrl = (state: string) => `${clientUrl}/login/${state}`;
