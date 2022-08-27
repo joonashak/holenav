6b6992abb665a7f140de352497ddd3fd0d11d271
joonashak <hakkijoo@gmail.com>
2022-08-27 14:15:45 +0300

    Add hook for authenticated REST API calls

---
+++ client/src/auth/useAuthenticatedRestApi.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import useUserSettings from "../components/UserData/settings/useUserSettings";
+import { backendUrl } from "../config";
+import useAuth from "./useAuth";
+
+const useAuthenticatedRestApi = () => {
+  const { token } = useAuth();
+  const { activeFolder } = useUserSettings();
+
+  const baseConfig: AxiosRequestConfig = {
+    headers: { accesstoken: token || "", activefolder: activeFolder.id },
+  };
+
+  const get = async (path: string): Promise<AxiosResponse> => {
+    const url = [backendUrl, path].join("/");
+    return axios.get(url, baseConfig);
+  };
+
+  return { get };
+};
+
+export default useAuthenticatedRestApi;
--- client/src/components/SystemView/Map/MapData/useMapData.ts
+++ client/src/components/SystemView/Map/MapData/useMapData.ts
@@ -1,19 +1,17 @@
 import { Downgraded, useState } from "@hookstate/core";
-import axios from "axios";
 import { mapState } from ".";
-import { backendUrl } from "../../../../config";
+import useAuthenticatedRestApi from "../../../../auth/useAuthenticatedRestApi";
 import useUserData from "../../../UserData/useUserData";
 
 export default () => {
   const state = useState(mapState);
   const { settings } = useUserData();
+  const { get } = useAuthenticatedRestApi();
   const { selectedMap } = settings;
 
   const fetchConnectionTree = async () => {
     const { rootSystemName } = selectedMap;
-    const res = await axios.get(
-      [backendUrl, "connection-graph", "connection-tree", rootSystemName].join("/")
-    );
+    const res = await get(["connection-graph", "connection-tree", rootSystemName].join("/"));
     state.merge({ connectionTree: res.data });
   };
 
