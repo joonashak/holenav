84be948f133ee3c52129361b60ad64ceaeb73273
joonashak <hakkijoo@gmail.com>
2022-08-28 11:19:37 +0300

    Fix graphql types

--- server/src/entities/signature/dto/add-signatures.dto.ts
+++ server/src/entities/signature/dto/add-signatures.dto.ts
@@ -1,7 +1,26 @@
 import { Field, InputType, ObjectType, OmitType } from "@nestjs/graphql";
+import MassStatus from "../enums/mass-status.enum";
 import { SignatureOLD } from "../signature-OLD.model";
-import { Signature } from "../signature.model";
+import { SignatureWithoutConnection } from "../signature.model";
 
+// FIXME: For whatever reason, using actual connection model fails.
+@InputType()
+export class ConnectionInput {
+  @Field({ nullable: true })
+  wormholeType?: string;
+
+  @Field()
+  eol: boolean;
+
+  @Field((type) => MassStatus)
+  massStatus: MassStatus;
+
+  @Field({ nullable: true })
+  destinationName?: string;
+
+  @Field({ nullable: true })
+  reverseType?: string;
+}
 @InputType()
 export class AddSignaturesInput {
   @Field((type) => [CreatableSignature])
@@ -9,7 +28,10 @@ export class AddSignaturesInput {
 }
 
 @InputType()
-export class CreatableSignature extends OmitType(Signature, ["id"]) {}
+export class CreatableSignature extends OmitType(SignatureWithoutConnection, ["id"]) {
+  @Field((type) => ConnectionInput, { nullable: true })
+  connection?: ConnectionInput;
+}
 
 @ObjectType()
 export class AddSignaturesOutput {
--- server/src/entities/signature/signature.model.ts
+++ server/src/entities/signature/signature.model.ts
@@ -1,11 +1,32 @@
-import { Field, ObjectType, registerEnumType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { Connection } from "./connection.model";
 import SigType from "./enums/sig-type.enum";
 
 registerEnumType(SigType, { name: "SigType" });
 
+// This split is done because a reference to another @InputType class with
+// duplicate field names breaks the GraphQL type system.
+@InputType()
+@InputType()
+export class SignatureWithoutConnection {
+  @Field()
+  id: string;
+
+  @Field()
+  eveId: string;
+
+  @Field((type) => SigType)
+  type: SigType;
+
+  @Field()
+  name: string;
+
+  @Field()
+  systemName: string;
+}
+
 @ObjectType()
-export class Signature {
+export class Signature extends SignatureWithoutConnection {
   @Field()
   id: string;
 
