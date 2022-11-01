5db148415cd1e7df2ec412c6fca3f2618ef42007
joonashak <hakkijoo@gmail.com>
2022-11-01 20:13:09 +0000

    Refactor

--- server/src/entities/signature/neo/signature-search.service.ts
+++ server/src/entities/signature/neo/signature-search.service.ts
@@ -12,9 +12,30 @@ export class SignatureSearchService {
   constructor(private neoService: Neo4jService) {}
 
   async findBySystem(params: SignatureSearchParams): Promise<Signature[]> {
-    const signatures = await this.findSignaturesBySystem(params);
-    const wormholes = await this.findWormholesBySystem(params);
-    return signatures.concat(wormholes);
+    const res = await this.neoService.read(
+      `
+      MATCH (origin:System {name: $systemName, folderId: $folderId})-[:HAS]-(sig:Signature)
+      OPTIONAL MATCH (sig)-[conn:CONNECTS]-(destSig:Signature)-[:HAS]-(dest:System)
+      RETURN
+      CASE
+        WHEN destSig IS NOT NULL THEN sig{
+          .*,
+          systemName: origin.name,
+          connection: conn{
+            .*,
+            reverseSignature: destSig{
+              .*,
+              systemName: dest.name
+            }
+          }
+        }
+        ELSE sig{.*, systemName: origin.name}
+      END AS signature
+      `,
+      params,
+    );
+
+    return res.records.map((rec) => rec._fields[0]);
   }
 
   async findManyById(ids: string[]): Promise<Signature[]> {
@@ -43,44 +64,5 @@ export class SignatureSearchService {
     );
 
     return res.records[0]._fields;
-  }
-
-  private async findSignaturesBySystem(params: SignatureSearchParams): Promise<Signature[]> {
-    const res = await this.neoService.read(
-      `
-      MATCH (:System {name: $systemName, folderId: $folderId})-[:HAS]->(sig:Signature)
-      WHERE sig.type <> 'WH'
-      WITH
-        sig{.*, systemName: $systemName} AS sigs
-      RETURN collect(DISTINCT sigs)
-    `,
-      params,
-    );
-
-    return res.records[0]._fields[0];
-  }
-
-  private async findWormholesBySystem(params: SignatureSearchParams): Promise<Signature[]> {
-    const res = await this.neoService.read(
-      `
-      MATCH (origin:System {name: $systemName, folderId: $folderId})-[:HAS]->(originSig:Signature)-[conn:CONNECTS]-(destSig:Signature)<-[:HAS]-(dest:System)
-      WITH
-        originSig{
-          .*,
-          systemName: origin.name,
-          connection: conn{ 
-            .*,
-            reverseSignature: destSig{
-              .*,
-              systemName: dest.name
-            }
-          } 
-        } AS wormholes
-      RETURN collect(DISTINCT wormholes)
-    `,
-      params,
-    );
-
-    return res.records[0]._fields[0];
   }
 }
