67d6aae29d74c45e30844ab067e07f6161f6371c
joonashak <hakkijoo@gmail.com>
2023-05-13 23:23:34 +0300

    Add ESI request methods for enriching character entities

--- server/src/esi/esi.service.ts
+++ server/src/esi/esi.service.ts
@@ -1,7 +1,39 @@
 import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
+import {
+  EsiAlliancePublicInfo,
+  EsiCharacterPortraitUrl,
+  EsiCharacterPublicInfo,
+  EsiCorporationPublicInfo,
+} from "./esi.types";
+
+const baseUrl = "https://esi.evetech.net";
 
 @Injectable()
 export class EsiService {
   constructor(private httpService: HttpService) {}
+
+  async getAlliancePublicInfo(allianceId: string) {
+    return this.httpService.get<EsiAlliancePublicInfo>(this.url("v4", "alliances", allianceId));
+  }
+
+  async getCharacterPortraitUrl(characterId: string) {
+    return this.httpService.get<EsiCharacterPortraitUrl>(
+      this.url("v3", "characters", characterId, "portrait"),
+    );
+  }
+
+  async getCharacterPublicInfo(characterId: string) {
+    return this.httpService.get<EsiCharacterPublicInfo>(this.url("v5", "characters", characterId));
+  }
+
+  async getCorporationPublicInfo(corporationId: string) {
+    return this.httpService.get<EsiCorporationPublicInfo>(
+      this.url("v5", "corporations", corporationId),
+    );
+  }
+
+  private url(...parts: string[]): string {
+    return `${[baseUrl, ...parts].join("/")}/`;
+  }
 }
---
+++ server/src/esi/esi.types.ts
@@ -0,0 +1,47 @@
+export type EsiAlliancePublicInfo = {
+  creator_corporation_id: number;
+  creator_id: number;
+  date_founded: string;
+  executor_corporation_id?: number;
+  faction_id?: number;
+  name: string;
+  ticker: string;
+};
+
+export type EsiCharacterPortraitUrl = {
+  px64x64: string;
+  px128x128: string;
+  px256x256: string;
+  px512x512: string;
+};
+
+export type EsiCharacterPublicInfo = {
+  alliance_id?: number;
+  birthday: string;
+  bloodline_id: number;
+  corporation_id: number;
+  description?: string;
+  faction_id?: number;
+  gender: string;
+  name: string;
+  race_id: number;
+  security_status?: number;
+  title?: string;
+};
+
+export type EsiCorporationPublicInfo = {
+  alliance_id?: number;
+  ceo_id: number;
+  creator_id: number;
+  date_founded?: string;
+  description?: string;
+  faction_id?: number;
+  home_station_id?: number;
+  member_count: number;
+  name: string;
+  shares?: number;
+  tax_rate: number;
+  ticker: string;
+  url?: string;
+  war_eligible?: boolean;
+};
