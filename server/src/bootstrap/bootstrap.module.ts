d0bda1ffe97651a3d18cab214828edc5a36d0323
joonashak <hakkijoo@gmail.com>
2023-05-19 11:24:50 +0300

    Refactor bootstrap service

--- server/src/bootstrap/bootstrap.module.ts
+++ server/src/bootstrap/bootstrap.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppDataModule } from "../app-data/app-data.module";
 import { BootstrapService } from "./bootstrap.service";
+import { AppUpdateService } from "./services/app-update.service";
 
 /**
  * Take care of stuff that needs to happen on server startup.
  */
-@Module({ imports: [AppDataModule], providers: [BootstrapService] })
+@Module({ imports: [AppDataModule], providers: [BootstrapService, AppUpdateService] })
 export class BootstrapModule {}
--- server/src/bootstrap/bootstrap.service.ts
+++ server/src/bootstrap/bootstrap.service.ts
@@ -1,79 +1,17 @@
 import { Injectable, Logger, OnApplicationBootstrap } from "@nestjs/common";
-import { request } from "@octokit/request";
-import { compare } from "compare-versions";
-import {
-  APP_VERSION,
-  CLIENT_CD_OWNER,
-  CLIENT_CD_REPO,
-  CLIENT_CD_TOKEN,
-  CLIENT_CD_WORKFLOW_ID,
-  DISABLE_CLIENT_CD_VERSION_INPUT,
-} from "../config";
-import { AppDataService } from "../app-data/app-data.service";
+import { AppUpdateService } from "./services/app-update.service";
 
 @Injectable()
 export class BootstrapService implements OnApplicationBootstrap {
   private readonly logger = new Logger(BootstrapService.name);
 
-  constructor(private appDataService: AppDataService) {}
+  constructor(private appUpdateService: AppUpdateService) {}
 
   async onApplicationBootstrap() {
-    if (await this.appUpdated()) {
+    if (await this.appUpdateService.appUpdated()) {
       this.logger.log("Application update detected.");
-      await this.updateAppDataVersion();
-      await this.triggerClientDeployment();
+      await this.appUpdateService.updateAppDataVersion();
+      await this.appUpdateService.triggerClientDeployment();
     }
-  }
-
-  private async appUpdated(): Promise<boolean> {
-    const { appVersion: oldVersion } = await this.appDataService.getAppData();
-
-    if (!oldVersion) {
-      return true;
-    }
-
-    return compare(APP_VERSION, oldVersion, ">");
-  }
-
-  private async updateAppDataVersion(): Promise<void> {
-    await this.appDataService.updateAppData({ appVersion: APP_VERSION });
-  }
-
-  /**
-   * Trigger client deployment using a GitHub Actions webhook.
-   */
-  private async triggerClientDeployment(): Promise<void> {
-    if (!CLIENT_CD_TOKEN) {
-      this.logger.log("Skipping client deployment (CLIENT_CD_TOKEN not set).");
-      return;
-    }
-
-    const url = "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches";
-
-    const inputs = DISABLE_CLIENT_CD_VERSION_INPUT
-      ? {}
-      : {
-          ref: `v${APP_VERSION}`,
-        };
-
-    const options = {
-      headers: {
-        authorization: `token ${CLIENT_CD_TOKEN}`,
-      },
-      owner: CLIENT_CD_OWNER,
-      repo: CLIENT_CD_REPO,
-      workflow_id: CLIENT_CD_WORKFLOW_ID,
-      ref: "main",
-      inputs,
-    };
-
-    try {
-      await request(url, options);
-    } catch (error) {
-      this.logger.warn("Request to client deployment webhook failed with the following error:");
-      throw error;
-    }
-
-    this.logger.log("Client deployment triggered.");
   }
 }
