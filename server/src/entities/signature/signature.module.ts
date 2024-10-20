06d51cfd2db919478c14feb185c4266a5261e8e3
joonashak <joonashak@proton.me>
2024-10-20 18:22:19 +0300

    Create connections when creating new wormhole sigs

--- server/src/entities/signature/signature.module.ts
+++ server/src/entities/signature/signature.module.ts
@@ -2,6 +2,7 @@ import { CloneBayUserService } from "@joonashak/nestjs-clone-bay";
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { FolderAccessControlModule } from "../../access-control/folder/folder-access-control.module";
+import { ConnectionModule } from "../connection/connection.module";
 import { FolderModule } from "../folder/folder.module";
 import { FolderService } from "../folder/folder.service";
 import { SignaturePasteService } from "./signature-paste.service";
@@ -13,6 +14,7 @@ import { SignatureService } from "./signature.service";
   imports: [
     FolderModule,
     FolderAccessControlModule,
+    ConnectionModule,
     MongooseModule.forFeature([
       { name: Signature.name, schema: SignatureSchema },
     ]),
--- server/src/entities/signature/signature.service.ts
+++ server/src/entities/signature/signature.service.ts
@@ -3,13 +3,16 @@ import { User } from "@joonashak/nestjs-clone-bay";
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { ConnectionService } from "../connection/connection.service";
 import { CreateSignature } from "./dto/add-signatures.dto";
+import SigType from "./enums/sig-type.enum";
 import { Signature } from "./signature.model";
 
 @Injectable()
 export class SignatureService {
   constructor(
     @InjectModel(Signature.name) private signatureModel: Model<Signature>,
+    private connectionService: ConnectionService,
   ) {}
 
   async getBySystem(
@@ -19,15 +22,29 @@ export class SignatureService {
     return [];
   }
 
+  /**
+   * Create signature and optional connection.
+   *
+   * If given signature is a wormhole, it must contain the
+   * `Signature.connection` object. The respective `Connection` document is
+   * created by this method and linked to the new `Signature`.
+   */
   async createSignature(
     signature: CreateSignature,
     user?: User,
   ): Promise<Signature> {
+    // Create new connection for wormholes.
+    const connection =
+      signature.type === SigType.WORMHOLE
+        ? await this.connectionService.create(signature.connection)
+        : null;
+
     const created = await this.signatureModel.create({
       ...signature,
-      connection: null,
+      connection,
       createdBy: user?.main.name || "",
     });
+
     return created;
   }
 
