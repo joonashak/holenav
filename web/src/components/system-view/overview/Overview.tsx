a9ea812e2d7399b5d08bef8204ee1c33b94b2087
joonashak <joonashak@proton.me>
2024-10-21 20:35:47 +0300

    Remove old overview menu

--- web/src/components/system-view/overview/Overview.tsx
+++ web/src/components/system-view/overview/Overview.tsx
@@ -1,47 +1,29 @@
-import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
-import { useState } from "react";
+import { AppBar, Box, Toolbar } from "@mui/material";
 import OverviewTitle from "./OverviewTitle";
 import IntelPanel from "./intel-panel/IntelPanel";
-import OverviewMenu from "./overview-menu/OverviewMenu";
 import ScanningPanel from "./scanning-panel/ScanningPanel";
 import SystemInfo from "./system-info/SystemInfo";
 
-const Overview = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => setMenuOpen((prev) => !prev);
-
-  return (
-    <Box
-      sx={{
-        zIndex: 1,
-        height: "fit-content",
-        width: {
-          xs: "100vw",
-          sm: "30rem",
-        },
-      }}
-    >
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            aria-label="Open Menu"
-            onClick={toggleMenu}
-            size="large"
-            data-cy="open-main-menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <OverviewMenu open={menuOpen} toggle={toggleMenu} />
-          <OverviewTitle />
-        </Toolbar>
-      </AppBar>
-      <SystemInfo />
-      <ScanningPanel />
-      <IntelPanel />
-    </Box>
-  );
-};
+const Overview = () => (
+  <Box
+    sx={{
+      zIndex: 1,
+      height: "fit-content",
+      width: {
+        xs: "100vw",
+        sm: "30rem",
+      },
+    }}
+  >
+    <AppBar position="static">
+      <Toolbar>
+        <OverviewTitle />
+      </Toolbar>
+    </AppBar>
+    <SystemInfo />
+    <ScanningPanel />
+    <IntelPanel />
+  </Box>
+);
 
 export default Overview;
