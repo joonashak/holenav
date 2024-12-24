51036d156db6aae26b011f2a60a406d2380a4b8b
joonashak <joonashak@proton.me>
2024-12-24 14:55:44 +0200

    Test creating unknown sig

--- testing/e2e/tests/example.spec.ts
+++ testing/e2e/tests/example.spec.ts
@@ -13,3 +13,15 @@ test("Create map", async ({ map }) => {
 test("test if map fixture breaks", async ({ map }) => {
   map.asd();
 });
+
+test("Create unknown signature", async ({ page }) => {
+  await page.goto("/system/J100001");
+  await page.getByRole("button", { name: "Add Signature" }).click();
+  await page.getByLabel("ID").fill("ASD-123");
+  await page.getByRole("button", { name: "Save Signature" }).click();
+
+  await expect(page.getByRole("alert")).toContainText("Signature added");
+  await expect(
+    page.getByRole("cell", { name: "ASD-123", exact: true }),
+  ).toBeVisible();
+});
