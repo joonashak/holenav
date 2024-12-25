4452f32a0ea1c017dbe887f8aca0e6afabacc28f
joonashak <joonashak@proton.me>
2024-12-25 09:34:46 +0200

    Run Playwright tests in parallel on CI using matrix strategy

--- testing/e2e/playwright.config.ts
+++ testing/e2e/playwright.config.ts
@@ -1,27 +1,20 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/** See https://playwright.dev/docs/test-configuration. */
+const ci = process.env.CI === "true";
+
 export default defineConfig({
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  forbidOnly: ci,
+  retries: ci ? 2 : 0,
+  workers: undefined,
+  reporter: ci ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:4000",
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
-
   /* Configure projects for major browsers */
   projects: [
     { name: "setup", testMatch: "**/*.setup.ts" },
@@ -40,29 +33,7 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
       dependencies: ["setup"],
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start:test",
     cwd: "../..",
