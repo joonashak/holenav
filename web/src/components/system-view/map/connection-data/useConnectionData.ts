26815877eeba894d81210cf5254ba8f32e8a1a13
joonashak <joonashak@proton.me>
2024-12-28 10:49:13 +0200

    Fix connection tree root system

--- web/src/components/system-view/map/connection-data/useConnectionData.ts
+++ web/src/components/system-view/map/connection-data/useConnectionData.ts
@@ -2,15 +2,17 @@ import { useQuery } from "@apollo/client";
 import { useMemo } from "react";
 import { FindConnectionGraphDocument } from "../../../../generated/graphqlOperations";
 import useActiveFolder from "../../../../hooks/useActiveFolder";
-import useCurrentSystemName from "../../useCurrentSystemName";
+import useSelectedMap from "../../../../hooks/useSelectedMap";
 import buildConnectionTree from "./build-connection-tree";
 
 const useConnectionData = () => {
-  const root = useCurrentSystemName();
+  const { selectedMap } = useSelectedMap();
+  const root = selectedMap?.rootSystemName || "";
   const { activeFolderId } = useActiveFolder();
 
   const { data } = useQuery(FindConnectionGraphDocument, {
     variables: { root, folderId: activeFolderId },
+    skip: root === "",
   });
 
   const connectionTree = useMemo(
