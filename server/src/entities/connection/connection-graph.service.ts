17b491ec0bd5271b2a8a629080ecd865dd4c0f4a
joonashak <joonashak@proton.me>
2025-01-06 19:11:04 +0200

    Add signature data to connection graph aggregation

--- server/src/entities/connection/connection-graph.service.ts
+++ server/src/entities/connection/connection-graph.service.ts
@@ -14,8 +14,10 @@ export class ConnectionGraphService {
     root: string,
     folderId: string,
   ): Promise<FindConnectionGraph> {
-    const chains = await this.connectionModel.aggregate([
+    const connections = await this.connectionModel.aggregate([
+      // Start with connections from given root system.
       { $match: { from: root, folderId } },
+      // Traverse the graph of all linked connections.
       {
         $graphLookup: {
           from: "connections",
@@ -28,11 +30,37 @@ export class ConnectionGraphService {
           restrictSearchWithMatch: { folderId },
         },
       },
+      // Pick only the first document as they all have identical connection lists due to the graph being two-way.
+      { $limit: 1 },
+      // Promote the connection list to documents.
+      { $unwind: { path: "$children" } },
+      { $replaceRoot: { newRoot: "$children" } },
+      // Add the related signature to each connection.
+      {
+        $lookup: {
+          from: "signatures",
+          localField: "_id",
+          foreignField: "connection",
+          as: "signature",
+        },
+      },
+      { $set: { signature: { $first: "$signature" } } },
+      // Copy IDs to match what Mongoose does automatically when not using aggregation pipelines.
+      { $addFields: { id: "$_id", "signature.id": "$signature._id" } },
+      // FIXME: This is not necessary after implementing reverse signatures.
+      // Remove empty signatures.
+      {
+        $addFields: {
+          signature: {
+            $cond: {
+              if: { $eq: ["$signature", {}] },
+              then: "$$REMOVE",
+              else: "$signature",
+            },
+          },
+        },
+      },
     ]);
-
-    const connections = chains.length
-      ? chains[0].children.map((conn) => ({ ...conn, id: conn._id }))
-      : [];
 
     return {
       root,
--- server/src/entities/connection/dto/graph-connection.dto.ts
+++ server/src/entities/connection/dto/graph-connection.dto.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType, OmitType } from "@nestjs/graphql";
+import { Signature } from "../../signature/signature.model";
 import { Connection } from "../connection.model";
 
 @ObjectType()
@@ -8,4 +9,11 @@ export class GraphConnection extends OmitType(Connection, ["reverse"]) {
 
   @Field()
   depth: number;
+
+  /**
+   * This really should not be nullable but will have to be as long as reverse
+   * signatures are not implemented.
+   */
+  @Field({ nullable: true })
+  signature?: Signature;
 }
