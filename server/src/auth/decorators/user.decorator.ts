0d48fa511f29f24c54321c1916800ed306169721
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2025-03-15 09:17:05 +0200

    Remove deprecated decorators

--- server/src/auth/decorators/user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import getRequest from "../utils/get-request.util";
-
-export const CurrentUser = createParamDecorator(
-  (_, context: ExecutionContext) => {
-    const request = getRequest(context);
-    return request.user;
-  },
-);
