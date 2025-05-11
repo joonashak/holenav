c74ea6353cc9db38abd6c5a7c8633b69fe016b3a
joonashak <joonashak@proton.me>
2025-05-11 12:52:45 +0300

    Seed database with connection graph before each test

--- testing/e2e/fixtures/auth.fixture.ts
+++ testing/e2e/fixtures/auth.fixture.ts
@@ -38,16 +38,22 @@ export const authFixture = test.extend<object, { account: Account }>({
       `http://localhost:4001/clone-bay-mocking/login?eveId=${eveId}`,
     );
 
-    // const folderName = `E2E Folder ${account.eveId}`;
-    // await page.goto("/system/J100001");
-    // await page.getByLabel("Open Settings Menu").click();
-    // await page.getByRole("menuitem", { name: "Folder Options" }).click();
-    // await page.getByRole("button", { name: "New Folder" }).click();
-    // await page.getByLabel("Name").fill(folderName);
-    // await page.getByRole("button", { name: "Create" }).click();
-    // await page.getByLabel("Active Folder").click();
-    // await page.getByRole("option", { name: folderName, exact: true }).click();
+    // TODO: This should be done with API to save time.
+    const folderName = `E2E Folder ${account.eveId}`;
+    await page.goto("/system/J100001");
+    await page.getByLabel("Open Settings Menu").click();
+    await page.getByRole("menuitem", { name: "Folder Options" }).click();
+    await page.getByRole("button", { name: "New Folder" }).click();
+    await page.getByLabel("Name").fill(folderName);
+    await page.getByRole("button", { name: "Create" }).click();
+    await page.getByLabel("Active Folder").click();
+    await page
+      .getByRole("option", { name: folderName, exact: true })
+      // FIXME: This will probably break stuff.
+      .first()
+      .click();
 
+    await page.goto("http://localhost:4001/dev/seed");
     await use(page);
   },
 });
