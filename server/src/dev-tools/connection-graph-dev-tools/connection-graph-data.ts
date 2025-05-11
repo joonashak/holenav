a9c586672a6882c89aa505391535501d050efa15
joonashak <joonashak@proton.me>
2025-05-11 17:21:38 +0300

    Refactor

--- server/src/dev-tools/connection-graph-dev-tools/connection-graph-data.ts
+++ server/src/dev-tools/connection-graph-dev-tools/connection-graph-data.ts
@@ -7,32 +7,87 @@ export type ConnectionGraphData = CreateMapDto & {
   signatures: CreateSignature[];
 };
 
+const wh = (
+  eveId: string,
+  name: string,
+  type: string,
+  from: string,
+  to: string | null = null,
+  { k162 = false, eol = false, massStatus = MassStatus.STABLE } = {},
+): CreateSignature => ({
+  type: SigType.WORMHOLE,
+  systemName: from,
+  eveId,
+  name,
+  connection: {
+    type,
+    from,
+    to,
+    k162,
+    eol,
+    massStatus,
+  },
+});
+
+const data = (
+  eveId: string,
+  systemName: string,
+  name = "",
+): CreateSignature => ({
+  type: SigType.DATA,
+  systemName,
+  eveId,
+  name,
+  connection: null,
+});
+
+const relic = (
+  eveId: string,
+  systemName: string,
+  name = "",
+): CreateSignature => ({
+  type: SigType.RELIC,
+  systemName,
+  eveId,
+  name,
+  connection: null,
+});
+
+const gas = (
+  eveId: string,
+  systemName: string,
+  name = "",
+): CreateSignature => ({
+  type: SigType.GAS,
+  systemName,
+  eveId,
+  name,
+  connection: null,
+});
+
+const unkn = (
+  eveId: string,
+  systemName: string,
+  name = "",
+): CreateSignature => ({
+  type: SigType.UNKNOWN,
+  systemName,
+  eveId,
+  name,
+  connection: null,
+});
+
 export const connectionGraphData: ConnectionGraphData[] = [
   {
-    name: "asd",
-    rootSystemName: "Jita",
+    name: "Small Tree",
+    rootSystemName: "J111141",
     signatures: [
-      {
-        type: SigType.DATA,
-        systemName: "Jita",
-        eveId: "DSA",
-        name: "",
-        connection: null,
-      },
-      {
-        type: SigType.WORMHOLE,
-        systemName: "Jita",
-        eveId: "FFF",
-        name: "hole",
-        connection: {
-          type: "C140",
-          from: "Jita",
-          to: "Amarr",
-          k162: false,
-          eol: false,
-          massStatus: MassStatus.STABLE,
-        },
-      },
+      data("JEI-927", "J111141"),
+      relic("OIE-128", "J111141", "Ruined Sansha Monument Site"),
+      gas("RWE-573", "J111141"),
+      unkn("JSO-882", "J111141"),
+      wh("NOW-820", "D2A NOW", "D382", "J111141", "J222822"),
+      wh("OAS-842", "ALA OAS", "A239", "J111141", "Tama"),
     ],
   },
 ];
