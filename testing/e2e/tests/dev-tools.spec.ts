d8450d972e292e25d66e9d420ff95158f0362da3
joonashak <joonashak@proton.me>
2025-05-11 18:03:51 +0300

    Fix dev tools test

--- testing/e2e/tests/dev-tools.spec.ts
+++ testing/e2e/tests/dev-tools.spec.ts
@@ -3,7 +3,7 @@ import { test } from "../fixtures/fixtures";
 
 // Smoke test for dev tools. Maybe temporary?
 test("Dev tools work", async ({ page }) => {
-  await page.goto("/system/Jita");
-  await expect(page.getByText("DSAData")).toBeVisible();
-  await expect(page.getByText("FFFC140hole")).toBeVisible();
+  await page.goto("/system/J111141");
+  await expect(page.getByText("NOW-820D382D2A")).toBeVisible();
+  await expect(page.getByText("OAS-842A239ALA")).toBeVisible();
 });
