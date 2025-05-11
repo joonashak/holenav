c948ca1f9b40c9b0acd85a25f24832957be23565
joonashak <joonashak@proton.me>
2025-05-11 12:38:28 +0300

    Don't attempt to drop collections that are dropped by library

--- server/src/dev-tools/dev-tools.service.ts
+++ server/src/dev-tools/dev-tools.service.ts
@@ -13,10 +13,14 @@ export class DevToolsService {
 
   private async dropAllCollections() {
     const collections = await this.dbConnection.listCollections();
+
+    // If `dynamic-config` is dropped, the server needs to be restarted.
+    // `users` and `sessions` are deleted by the library.
+    const skipCollections = ["dynamic-config", "users", "sessions"];
+
     await Promise.all(
       collections
-        // If `dynamic-config` is dropped, the server needs to be restarted.
-        .filter((collection) => collection.name !== "dynamic-config")
+        .filter((collection) => !skipCollections.includes(collection.name))
         .map((collection) => this.dbConnection.dropCollection(collection.name)),
     );
   }
