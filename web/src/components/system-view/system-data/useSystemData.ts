a56459bf577c8a66015dbac939a923d0e83681d8
joonashak <joonashak@proton.me>
2025-05-13 20:38:01 +0300

    Update system data hook mayhem to use Zustand

--- web/src/components/system-view/system-data/useSystemData.ts
+++ web/src/components/system-view/system-data/useSystemData.ts
@@ -1,44 +1,40 @@
-import { Downgraded, useState } from "@hookstate/core";
-import { systemState } from "./SystemData";
+import { findOneSystem, System, WormholeEffect } from "@eve-data/systems";
+import { create } from "zustand";
+import SecurityClasses from "../../../enum/SecurityClasses";
 
-export default () => {
-  const state = useState(systemState);
-
-  const changeSystem = (name: string) => {
-    state.merge({ name });
-  };
-
-  return {
-    get id() {
-      return state.id.get();
-    },
-    get eveId() {
-      return state.eveId.get();
-    },
-    get name() {
-      return state.name.get();
-    },
-    get securityClass() {
-      return state.securityClass.get();
-    },
-    get securityStatus() {
-      return state.securityStatus.get();
-    },
-    get whClass() {
-      return state.whClass.get();
-    },
-    get effect() {
-      return state.effect.get();
-    },
-    get region() {
-      return state.region.get();
-    },
-    get constellation() {
-      return state.constellation.get();
-    },
-    get staticConnections() {
-      return state.staticConnections.attach(Downgraded).get();
-    },
-    changeSystem,
-  };
+type SystemData = {
+  eveId: number;
+  name: string;
+  securityClass: SecurityClasses;
+  securityStatus: number;
+  whClass: number | null;
+  effect: WormholeEffect | null;
+  region: System["region"];
+  constellation: System["constellation"];
+  staticConnections: System["staticConnections"];
 };
+
+type SystemDataActions = {
+  changeSystem: (name: string) => void;
+};
+
+const { id, ...data } = findOneSystem({ name: "Jita" });
+const defaultSystem = {
+  ...data,
+  eveId: id,
+  securityClass: data.securityClass as SecurityClasses,
+};
+
+const useSystemData = create<SystemData & SystemDataActions>((set) => ({
+  ...defaultSystem,
+  changeSystem: (name) => {
+    const { id, ...data } = findOneSystem({ name });
+    set({
+      ...data,
+      eveId: id,
+      securityClass: data.securityClass as SecurityClasses,
+    });
+  },
+}));
+
+export default useSystemData;
